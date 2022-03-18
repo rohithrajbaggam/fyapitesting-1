b906f69c27b339daeff353ac80535af31797ceef
rohithrajbaggam <baggamrohithraj@gmail.com>
2022-03-18 23:08:19 +0530

    fyapitesting-1

--- fyapi/settings.py
+++ fyapi/settings.py
@@ -37,14 +37,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'pages.apps.PagesConfig',
     'users.apps.UsersConfig',
+    'userapp.apps.UserappConfig',
+
     'djoser',
     'rest_framework',
-    'userapp.apps.UserappConfig',
+    "corsheaders",
 ] 
 
 MIDDLEWARE = [
+
+    "corsheaders.middleware.CorsMiddleware",
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -165,3 +171,11 @@ SIMPLE_JWT = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# CORS_ALLOWED_ORIGINS = [
+#     "https://example.com",
+#     "https://sub.example.com",
+#     "http://localhost:8080",
+#     "http://127.0.0.1:9000",
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True
--- pages/views.py
+++ pages/views.py
@@ -8,7 +8,7 @@ def getRoutes(request):
     routes = [ 
         'http://127.0.0.1:8000/auth/users/', # user registration 
         'http://127.0.0.1:8000/auth/jwt/create', # login a user to get access and refresh token 
-        'userapp/userprofile/me/', # request user profile, GET,POST, PUT, request
+        'http://127.0.0.1:8000/userapp/userprofile/me/', # request user profile, GET,POST, PUT, request
         'http://127.0.0.1:8000/auth/jwt/refresh', # to get access token from refresh token
         'http://127.0.0.1:8000/userapp/userprofile/', # user profile
 
